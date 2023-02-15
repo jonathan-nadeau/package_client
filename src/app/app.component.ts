8a99e5f7ec458e39b563467ab851aa43800c6bb5
jnadeau22 <jnadeau@mcjconseil.com>
2023-02-15 08:34:59 -0500

    create navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Package } from './models';
+import { PackageService } from './services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  _packages: Package[] = [];
+
+  constructor(private packageService: PackageService) {}
+
+  getPackages(): void {
+    this.packageService
+      .getPackages()
+      .subscribe((result) => (this._packages = result));
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 /**
  * Components
@@ -19,6 +20,8 @@ import { PackageComponent } from './views/package/package.component';
  * Services
  */
 import { EstablishmentService, PackageService } from './services';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,8 +30,15 @@ import { EstablishmentService, PackageService } from './services';
     AboutComponent,
     AdminComponent,
     PackageComponent,
+    NavbarComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+  ],
   providers: [PackageService, EstablishmentService],
   bootstrap: [AppComponent],
 })
